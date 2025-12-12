77bf4048aa030fa5b7082f8c469e9892ded64dcb
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-12-12 15:28:40 -0300

    [feature] Engine control setup completed

--- EMCUA/motor/motor-controller.h
+++ EMCUA/motor/motor-controller.h
@@ -4,42 +4,25 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define clockwise_PIN PD3        // CH5 - 5 - PIN_3
 #define counterclockwise_PIN PD5 // CH5 - 6 - PIN_5
 
+#define FREQ_HZ 1000
+#define PERIOD_MS (1000/FREQ_HZ)
+
 #define ON 1
 #define OFF 0
 
-/**
- *
- */
-typedef struct {
-  uint8_t direction;      // Motor direction (0: forward, 1: reverse)
-  uint8_t pwm_duty1;     // PWM duty cycle for pin 1
-  uint8_t pwm_duty2;     // PWM duty cycle for pin 2
-  uint8_t enabled;       // Motor enable state
-} Motor_Config;
-
-/**
- *
- */
 void motorInit(void);
 
-/**
- *
- */
 void motorWrite(int8_t direction, uint8_t powerMode);
-
-void motorRamp(float v_init, float v_target, uint16_t accel_time_ms);
-
-uint8_t voltageToDuty(float voltage);
 
 void applyPWM(int8_t direction, uint8_t duty);
 
-void pwmSetDutyPercent_PD3(uint8_t percent);
-void pwmSetDutyPercent_PD5(uint8_t percent);
+uint8_t voltageToDuty(float voltage);
 
-void pwmInit(void);
+void motorRamp(float v_init, float v_target, uint16_t accel_time_ms);
 
 #endif /* MOTOR_CONTROLLER_H_ */
