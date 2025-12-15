6a8d33738022d9dc36801379097c2121ddf74070
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-12-14 22:53:59 -0300

    [feature] Added sample file for use with the EMCUA library

--- EMCUA/main.c
+++ EMCUA/main.c
@@ -1,47 +1,52 @@
 #include <xc.h>
 #include "EMCUA.h"
 
-int main (){
-  setup_LCD_serial_COM();
-	uint16_t number = 0;
-	char text [16] = "Velocidade:     "; 
-	while(1){	
-    send_LCD_label_value(text,number, 2);
-    number ++;
-    _delay_ms(100);
-  }
-}
-
-/*
 int main(void){
   motorInit();
   pwmInit();
   sensorInit();
-  uartInit(74880);
   millisInit();
   adcInit();
+  uartInit(74880);
+  setup_LCD_serial_COM();
+  
+  applyPWM(1, 60); // start Motor
+  _delay_ms(100);
 
-  applyPWM(1, 100);
+  unsigned long rpm;
+  uint8_t duty;
 
+  char MA [16] = "MA   -  DT:     ";
+  char MF [16] = "MF   -  DT:     ";
+  char GR [16] = "SPEED:          "; // General response
   while (1) {
+    uint8_t response = uartCommandTask();
+
     uint16_t val = adcRead();
-    if (val < THRESHOLD) { // detecta descida
+    if (val < THRESHOLD) {
       if (firstFall) {
-        // primeira descida: apenas resetar
         resetMillis();
         firstFall = 0;
       } else {
-        // segunda ou subsequente: calcula período
         lastPeriod = millis();
-        //uartSendInt(lastPeriod);
-        unsigned long rpm = periodToRPM(lastPeriod);
-        //uartSendInt(lastPeriod);
-        uartSendInt(rpm); 
+        rpm = periodToRPM(lastPeriod);
+
+        if(response == MA_RESPONSE){ // Open-loop control
+          send_LCD_label_value(MA, rpm, 1);
+        }
+        else if(response == MF_RESPONSE){ // Closed-loop control
+          duty = applyControl(rpm);
+          applyPWM(1, duty);
+          storeValues(rpm, duty);
+          uartSendInt(rpm);
+          send_LCD_label_value(MF, rpm, 1);
+        }
+        
+        send_LCD_label_value(GR, rpm, 2);
+
         resetMillis();
       }
-      // espera subir antes de detectar nova descida
       while (adcRead() < THRESHOLD);
     }
   }
 }
-*/
